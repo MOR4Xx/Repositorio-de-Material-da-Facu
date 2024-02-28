080379e0d3d484d2acbb55d2a364e0cb6fa03676
Jorge Afonso <128873434+MOR4Xx@users.noreply.github.com>
2024-02-27 22:29:46 -0300

    .

---
+++ Lista.java
@@ -0,0 +1,90 @@
+package ListaDinamica;
+
+
+public class Lista {
+    private static class No {
+        Object item;
+        No prox;
+    }
+
+    private No primeiro;
+    private No ultimo;
+
+    public Lista() {
+        primeiro = new No();
+        ultimo = primeiro;
+        primeiro.prox = null;
+    }
+
+    public void imprimirList() {
+        No atual = primeiro;
+        do {
+            System.out.println(atual.item);
+            atual = atual.prox;
+        } while (atual != null);
+
+    }
+
+    public void inseriFinal(Object x) {
+
+        ultimo.prox = new No();
+        ultimo = ultimo.prox;
+        ultimo.item = x;
+        ultimo.prox = null;
+    }
+
+
+    //Exercicio 03
+    public void concatenarListas(Lista lista1, Lista lista2) {
+        No atual = lista1.primeiro;
+        Lista list3 = new Lista();
+
+        while (atual != null) {
+            list3.inseriFinal(atual.item);
+            atual = atual.prox;
+        }
+
+        atual = lista2.primeiro.prox;
+        while (atual != null) {
+            list3.inseriFinal(atual.item);
+            atual = atual.prox;
+        }
+
+        list3.imprimirList();
+    }
+
+    //Exercicio 6
+    public void trocarDoisElementos(Object no1, Object no2) {
+        No aux1 = null, aux2 = null;
+        No atual = primeiro;
+
+        while (atual.prox != null){
+            if (atual.prox.item.equals(no1)) aux1 = atual;
+            if (atual.prox.item.equals(no2)) aux2 = atual;
+
+            atual = atual.prox;
+        }
+
+        if (no1 != null && no2 != null){
+            No temp = aux1.prox;
+            aux1 = aux2.prox;
+            aux2 = temp;
+            System.out.println("Troca Bem sucedida!!");
+        }
+    }
+    
+    //Exercicio 7
+    public void listaPar(Lista list1){
+        No atual = list1.primeiro.prox;
+        Lista list2 = new Lista();
+
+        while (atual != null ) {
+            if ((Integer)atual.item % 2 == 0) {
+                list2.inseriFinal(atual.item);
+            }
+            atual = atual.prox;
+        }
+        list2.imprimirList();
+    }
+
+}
