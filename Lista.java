8758983076846043f00ff40a533d219b6f008f47
Jorge Afonso <128873434+MOR4Xx@users.noreply.github.com>
2024-02-28 10:48:25 -0300

    ..

--- Lista.java
+++ Lista.java
@@ -1,5 +1,4 @@
-package ListaDinamica;
-
+package AulasED1;
 
 public class Lista {
     private static class No {
@@ -17,7 +16,7 @@ public class Lista {
     }
 
     public void imprimirList() {
-        No atual = primeiro;
+        No atual = primeiro.prox;
         do {
             System.out.println(atual.item);
             atual = atual.prox;
@@ -33,8 +32,7 @@ public class Lista {
         ultimo.prox = null;
     }
 
-
-    //Exercicio 03
+    // Exercicio 03
     public void concatenarListas(Lista lista1, Lista lista2) {
         No atual = lista1.primeiro;
         Lista list3 = new Lista();
@@ -53,33 +51,35 @@ public class Lista {
         list3.imprimirList();
     }
 
-    //Exercicio 6
+    // Exercicio 6
     public void trocarDoisElementos(Object no1, Object no2) {
         No aux1 = null, aux2 = null;
         No atual = primeiro;
 
-        while (atual.prox != null){
-            if (atual.prox.item.equals(no1)) aux1 = atual;
-            if (atual.prox.item.equals(no2)) aux2 = atual;
+        while (atual.prox != null) {
+            if (atual.prox.item.equals(no1))
+                aux1 = atual;
+            if (atual.prox.item.equals(no2))
+                aux2 = atual;
 
             atual = atual.prox;
         }
 
-        if (no1 != null && no2 != null){
+        if (no1 != null && no2 != null) {
             No temp = aux1.prox;
             aux1 = aux2.prox;
             aux2 = temp;
             System.out.println("Troca Bem sucedida!!");
         }
     }
-    
-    //Exercicio 7
-    public void listaPar(Lista list1){
+
+    // Exercicio 7
+    public void listaPar(Lista list1) {
         No atual = list1.primeiro.prox;
         Lista list2 = new Lista();
 
-        while (atual != null ) {
-            if ((Integer)atual.item % 2 == 0) {
+        while (atual != null) {
+            if ((Integer) atual.item % 2 == 0) {
                 list2.inseriFinal(atual.item);
             }
             atual = atual.prox;
@@ -87,4 +87,44 @@ public class Lista {
         list2.imprimirList();
     }
 
+    // Exercicio 9
+    public void ordemLista() {
+        No atual = primeiro.prox;
+        boolean crescente = false, decrescente = false;
+        while (atual.prox != null) {
+            if (atual.prox != null && ((Integer) atual.item < (Integer) atual.prox.item)) {
+                decrescente = false;
+                break;
+            } else
+                decrescente = true;
+
+            atual = atual.prox;
+        }
+        while (atual.prox != null) {
+            if (atual.prox != null && ((Integer) atual.item > (Integer) atual.prox.item)) {
+                crescente = false;
+                break;
+            } else
+                crescente = true;
+            atual = atual.prox;
+        }
+        if (decrescente || crescente)
+            System.out.println("Lista está ordenada.");
+        else
+            System.out.println("Não é ordenada");
+    }
+
+    // Exercicio 19
+    
+    // Exercicio 31
+    public void somaLista() {
+        No atual = primeiro.prox;
+        Integer soma = 0;
+
+        while (atual != null) {
+            soma += (Integer) atual.item;
+            atual = atual.prox;
+        }
+        System.out.println("A soma é: " + soma);
+    }
 }
--- Teste.java
+++ Teste.java
@@ -1,22 +1,23 @@
-package ListaDinamica;
+package AulasED1;
 
 public class Teste {
     public static void main(String[] args) {
 
         Lista list1 = new Lista();
+        list1.inseriFinal(1);
+        list1.inseriFinal(3);
+        list1.inseriFinal(5);
+        list1.inseriFinal(6);
+        list1.inseriFinal(10);
         list1.inseriFinal(12);
-        list1.inseriFinal(100);
-        list1.inseriFinal(19000);
-        list1.inseriFinal(15);
-        list1.inseriFinal(159);
-        list1.inseriFinal(548967);
-        list1.inseriFinal(120);
-        // list1.inseriFinal(11);
+        //list1.inseriFinal(11);
 
         list1.imprimirList();
         System.out.println();
 
-        list1.listaPar(list1);
+        list1.ordemLista();
+        //list1.somaLista();
+        // list1.listaPar(list1);
 
 
         // Lisa list2 = new Lista();
